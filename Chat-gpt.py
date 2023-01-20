d2e9b72cb87ebebe20ff908175b079c66930eaf5
omer-X-kiyak <89635390+omer-X-kiyak@users.noreply.github.com>
2023-01-20 21:37:36 +0300

    Update Chat-gpt.py

--- Chat-gpt.py
+++ Chat-gpt.py
@@ -1,25 +1,19 @@
 from transformers import AutoModelWithLMHead, AutoTokenizer
+import torch
 
-# Load the model and tokenizer
-model = AutoModelWithLMHead.from_pretrained("microsoft/DialoGPT-small")
-tokenizer = AutoTokenizer.from_pretrained("microsoft/DialoGPT-small")
+# Önceden eğitilmiş model ve tokenizer'i yükle
+model = AutoModelWithLMHead.from_pretrained("microsoft/DialoGPT-large")
+tokenizer = AutoTokenizer.from_pretrained("microsoft/DialoGPT-large")
 
 def generate_response(prompt):
-  # Encode the prompt and generate a response
-  input_ids = tokenizer.encode(prompt, return_tensors='pt').to('cuda')
-  response = model.generate(input_ids, max_length=1000, temperature=0.7)
-  response_text = tokenizer.decode(response[0], skip_special_tokens=True)
+    # Modeline girdi olarak prompt'u kodla
+    input_ids = torch.tensor(tokenizer.encode(prompt)).unsqueeze(0)
+    # Cevap üret
+    response = model.generate(input_ids, max_length=1024)
+    # Cevabı kodlamadan döndür
+    return tokenizer.decode(response[0], skip_special_tokens=True)
 
-  # Remove the prompt from the response
-  prompt_len = len(tokenizer.encode(prompt, return_tensors='pt')[0])
-  response_text = response_text[prompt_len:]
-
-  return response_text
-
-# Test the chatbot
-while True:
-    question = input("You: ")
-    if question == "exit":
-        break
-    answer = generate_response(question)
-    print(f"Bot: {answer}")
+# Örnek kullanım
+prompt = "Merhaba, bugün size nasıl yardımcı olabilirim?"
+response = generate_response(prompt)
+print(response)
